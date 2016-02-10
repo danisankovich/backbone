dc44f7bba6cd4eb36b7e965533b197ecc2c4d72c
Michael <misankovich@gmail.com>
2016-02-10 10:14:57 -0500

    updating the dom

--- js/main.js
+++ js/main.js
@@ -1,78 +1,21 @@
-//passing model to views
-// var Song = Backbone.Model.extend()
-// var SongView = Backbone.View.extend({
-//   render: function() {
-//     this.$el.html(this.model.get("title"))
-//     return this;
-//   }
-// })
-// var song = new Song({title: "Teenagers"})
-// var songView = new SongView({ el: "#container", model: song});
-// songView.render();
+//updating the view
 
-
-// =======================
-// =======================
-// =======================
-// =======================
-//passing collection to view
-
-// var Song = Backbone.Model.extend()
-// var Songs = Backbone.Collection.extend({
-//   model: Song
-// })
-// var SongView = Backbone.View.extend({
-//   tagName: "li",
-//   render: function() {
-//     this.$el.html(this.model.get("title"))
-//     return this;
-//   }
-// })
-// var SongsView = Backbone.View.extend({
-//   render: function() {
-//     var self = this
-//     this.model.each(function(song) { //refers to collection passed to view
-//       var songView = new SongView({ model: song}); //iteration passes in the current song
-//       self.$el.append(songView.render().$el)//self so that scope is not lost
-//     })
-//   }
-// })
-// var songs = new Songs([
-//   new Song({title:"Teenagers"}),
-//   new Song({title:"So What"}),
-//   new Song({title:"Summer Song"})
-// ])
-// var songsView = new SongsView({ el: "#songs", model: songs});
-// songsView.render();
-
-// =========================================
-// =========================================
-// =========================================
-// =========================================
-// =========================================
-//event handlers
-
-var Song = Backbone.Model.extend()
+var Song = Backbone.Model.extend({
+  defaults: {
+    listeners: 0
+  }
+});
 
 var SongView = Backbone.View.extend({
-  events: {
-    "click": "onClick",
-    // "click :not(.bookmark)": "onClick",  //for clicking on all buttons without class bookmark
-    "click .bookmark": "onClickBookmark"
+  initialize: function() { //when something changes (like a new listener), it will update the dom
+    this.model.on("change", this.render, this);//on chnage, register the render method, set the context (in this case, to the view itself when the render method is called)
   },
-  onClick: function() {
-    console.log('button clicked')
-  },
-  onClickBookmark: function() {
-    console.log('bookmark clicked')
-  },
-  tagName: "li",
   render: function() {
-    this.$el.html(this.model.get("title") + " <button>Listen</button><button class='bookmark'>Bookmark</button>")
-    return this;
+    this.$el.html(this.model.get("title") + " - Listeners: " + this.model.get("listeners"));
+    return this
   }
-})
+});
 
-var song = new Song({ title:"Teenagers"})
+var song = new Song({title: "Teenagers"});
 var songView = new SongView({ el: "#songs", model: song});
 songView.render();
--- js/view.js
+++ js/view.js
@@ -39,3 +39,89 @@ var songView = new SongView();
 // songView.render();
 // $("#container").html(songView.$el)
 $("#container").html(songView.render().$el)
+
+
+//=========================================
+//=========================================
+//=========================================
+//=========================================
+
+//passing model to views
+// var Song = Backbone.Model.extend()
+// var SongView = Backbone.View.extend({
+//   render: function() {
+//     this.$el.html(this.model.get("title"))
+//     return this;
+//   }
+// })
+// var song = new Song({title: "Teenagers"})
+// var songView = new SongView({ el: "#container", model: song});
+// songView.render();
+
+
+// =======================
+// =======================
+// =======================
+// =======================
+//passing collection to view
+
+// var Song = Backbone.Model.extend()
+// var Songs = Backbone.Collection.extend({
+//   model: Song
+// })
+// var SongView = Backbone.View.extend({
+//   tagName: "li",
+//   render: function() {
+//     this.$el.html(this.model.get("title"))
+//     return this;
+//   }
+// })
+// var SongsView = Backbone.View.extend({
+//   render: function() {
+//     var self = this
+//     this.model.each(function(song) { //refers to collection passed to view
+//       var songView = new SongView({ model: song}); //iteration passes in the current song
+//       self.$el.append(songView.render().$el)//self so that scope is not lost
+//     })
+//   }
+// })
+// var songs = new Songs([
+//   new Song({title:"Teenagers"}),
+//   new Song({title:"So What"}),
+//   new Song({title:"Summer Song"})
+// ])
+// var songsView = new SongsView({ el: "#songs", model: songs});
+// songsView.render();
+
+// =========================================
+// =========================================
+// =========================================
+// =========================================
+// =========================================
+//event handlers
+
+var Song = Backbone.Model.extend()
+
+var SongView = Backbone.View.extend({
+  events: {
+    "click": "onClick",
+    // "click :not(.bookmark)": "onClick",  //for clicking on all buttons without class bookmark
+    "click .bookmark": "onClickBookmark"
+  },
+  onClick: function() {
+    console.log('button clicked')
+  },
+  onClickBookmark: function(e) {
+    e.stopPropagation() // makes sure that only the relevent click event takes place.
+    console.log('bookmark clicked')
+  },
+  tagName: "li",
+  render: function() {
+    this.$el.html(this.model.get("title") + " <button>Listen</button><button class='bookmark'>Bookmark</button>")
+    return this;
+  }
+})
+
+var song = new Song({ title:"Teenagers"})
+var songView = new SongView({ el: "#songs", model: song});
+songView.render();
