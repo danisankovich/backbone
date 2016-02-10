c5b917fb0f62471f6a20eab004c53602823f22c9
Michael <misankovich@gmail.com>
2016-02-10 00:14:40 -0500

    passing collections to views

--- js/main.js
+++ js/main.js
@@ -1,41 +1,46 @@
-//views
-//responsible for rendering the content
-//and handling changes in the model and handling the dom events
-//more like controllers in MVC. have a referemce to a DOM element
-
+//passing model to views
+// var Song = Backbone.Model.extend()
 // var SongView = Backbone.View.extend({
 //   render: function() {
-//     this.$el.html("hello World!")
+//     this.$el.html(this.model.get("title"))
 //     return this;
 //   }
 // })
-// var songView = new SongView({ el: "#container"});
+// var song = new Song({title: "Teenagers"})
+// var songView = new SongView({ el: "#container", model: song});
 // songView.render();
-//or
-// var SongView = Backbone.View.extend({
-//   render: function() {
-//     this.$el.html("hello World!")
-//     return this;
-//   }
-// })
-// var songView = new SongView();
-// songView.render();
-// $("#container").html(songView.$el)
 
 
+// =======================
+// =======================
+// =======================
+// =======================
+//passing collection to view
+
+var Song = Backbone.Model.extend()
+var Songs = Backbone.Collection.extend({
+  model: Song
+})
 var SongView = Backbone.View.extend({
-  tagName: "span",
-  className: "song",
-  id: "1234",
-  attributes: {
-    "data-genre": "Rock"
-  },
+  tagName: "li",
   render: function() {
-    this.$el.html("hello World!")
+    this.$el.html(this.model.get("title"))
     return this;
   }
 })
-var songView = new SongView();
-// songView.render();
-// $("#container").html(songView.$el)
-$("#container").html(songView.render().$el)
+var SongsView = Backbone.View.extend({
+  render: function() {
+    var self = this
+    this.model.each(function(song) { //refers to collection passed to view
+      var songView = new SongView({ model: song}); //iteration passes in the current song
+      self.$el.append(songView.render().$el)//self so that scope is not lost
+    })
+  }
+})
+var songs = new Songs([
+  new Song({title:"Teenagers"}),
+  new Song({title:"So What"}),
+  new Song({title:"Summer Song"})
+])
+var songsView = new SongsView({ el: "#songs", model: songs});
+songsView.render();
